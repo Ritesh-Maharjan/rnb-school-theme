4e4cca6cc9fff4a5b7cbd6b9a9eb70acf2539389
Nikko <nikkobondoc1120@gmail.com>
2024-03-05 15:25:00 -0800

    removed sidebar from archive-rnb-studetns and changed title name

--- archive-rnb-students.php
+++ archive-rnb-students.php
@@ -16,7 +16,7 @@ get_header();
 
     <header class="page-header">
         <?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				post_type_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
     </header><!-- .page-header -->
@@ -25,14 +25,15 @@ get_header();
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
+				
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
-
+				
+				
 			endwhile;
 
 			the_posts_navigation();
@@ -47,5 +48,4 @@ get_header();
 </main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
