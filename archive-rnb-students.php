1e44da4a105f459c00411bd64a29ee30a6f08b6f
Nikko <nikkobondoc1120@gmail.com>
2024-03-06 18:48:48 -0800

    fixed links for students page

--- archive-rnb-students.php
+++ archive-rnb-students.php
@@ -25,25 +25,30 @@ get_header();
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-				
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-page', get_post_type() );
-				
-				
-			endwhile;
+				?>
+    <a href="<?php the_permalink(); ?>">
+        <h2><?php the_title(); ?></h2>
 
-			the_posts_navigation();
+    </a>
+    <?php
+    /*
+    * Include the Post-Type-specific template for the content.
+    * If you want to override this in a child theme, then include a file
+    * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+    */
+    get_template_part( 'template-parts/content-page', get_post_type() );
 
-		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+    endwhile;
 
-		endif;
-		?>
+    the_posts_navigation();
+
+    else :
+
+    get_template_part( 'template-parts/content', 'none' );
+
+    endif;
+    ?>
 
 </main><!-- #main -->
 
