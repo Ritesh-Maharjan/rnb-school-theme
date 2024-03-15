d5d4ca89396932d3983ed7498b401113d03ecabe
Ritesh <riteshmaharjan07@gmail.com>
2024-03-14 17:40:58 -0700

    Finished staff page

--- page-staff.php
+++ page-staff.php
@@ -14,81 +14,101 @@
 
 get_header();
 ?>
-	<main id="primary" class="site-main">
-		
-    
-		<?php
-		
+<main id="primary" class="site-main">
 
-			get_template_part( 'template-parts/content', 'page' );
-			
-		
-			$terms = get_terms( 
-				array(
-					'taxonomy' => 'rnb-staff',
-					'hide_empty' => false,
-				) 
+
+	<h2>
+		<?php the_title() ?>
+	</h2>
+
+	<?php
+
+
+	get_template_part('template-parts/content', 'page');
+
+
+	$terms = get_terms(
+		array(
+			'taxonomy' => 'rnb-staff',
+			'hide_empty' => false,
+		)
+	);
+	if ($terms && !is_wp_error($terms)) {
+		foreach ($terms as $term) {
+			$args = array(
+				'post_type' => 'rnb-staff',
+				'posts_per_page' => -1,
+				'orderby' => 'title',
+				'order' => 'ASC',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'rnb-staff',
+						'field' => 'slug',
+						'terms' => $term
+					),
+				),
 			);
-			if ( $terms && ! is_wp_error( $terms ) ) {
-				foreach ($terms as $term) {
-					$args = array(
-						'post_type' => 'rnb-staff',
-						'posts_per_page' => -1,
-						'orderby' => 'title',
-						'order' => 'ASC',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'rnb-staff',
-								'field' => 'slug',
-								'terms' => $term
-							),
-						),
-					);
-					$query = new WP_Query($args);
-					echo '<section class=""><h2>' . esc_html__($term->name, 'rnb') . '</h2>';
-				
-					
-					// creating our services
-					while ($query->have_posts()) {
-						$query->the_post();
-						?>
-                        
-						<article id="<?php echo get_the_ID(); ?>">
-							<h2>
-								<?php the_title(); ?>
-							</h2>
-							<?php
-							// ACF form validation
-							if (function_exists('get_field')) {
-								if (get_field('staff')) {
-									echo the_field('staff');
-									echo '<br>';
-								}
-                                if (get_field('courses')) {
-									echo the_field('courses');
-									echo '<br>';
-								}
-								
-                                $link = get_field('url');
-                                if( $link ): ?>
-                                    <a class="button" href="<?php echo esc_url( $link ); ?>">Instructor Website</a>
-                                <?php endif; 
-							}
-							?>
-                            
-						</article>
-						<?php
-					}
-					wp_reset_postdata();
-					// Add your WP_Query() code here
-					// Use $term->slug in your tax_query
-					// Use $term->name to organize the posts
-				}
-			}
-			
-		?>	
 
-	</main><!-- #primary -->
+			?>
+			<section class="staff-container">
+				<?php
+				$query = new WP_Query($args);
+				echo '<h2>' . esc_html__($term->name, 'rnb') . '</h2>';
+
+
+				// creating our services
+				while ($query->have_posts()) {
+					$query->the_post();
+					?>
+
+					<article id="<?php echo get_the_ID(); ?>">
+						<h2>
+							<?php the_title(); ?>
+						</h2>
+						<?php
+						// ACF form validation
+						if (function_exists('get_field')) {
+							if (get_field('staff')) {
+								?>
+								<p>
+									<?php the_field('staff') ?>
+								</p>
+								<?php
+							}
+							if (get_field('courses')) {
+								?>
+								<span>
+									<?php the_field('courses') ?>
+								</span>
+								<br />
+								<?php
+							}
+
+							$link = get_field('url');
+							if ($link): ?>
+								<a class="button" href="<?php echo esc_url($link); ?>">Instructor Website</a>
+							<?php endif;
+						}
+						?>
+
+					</article>
+					<?php
+				}
+				wp_reset_postdata();
+				?>
+
+			</section>
+			<?php
+
+			// Add your WP_Query() code here
+			// Use $term->slug in your tax_query
+			// Use $term->name to organize the posts
+		}
+	}
+
+	?>
+
+</main><!-- #primary -->
 
 <?php
 get_footer();
